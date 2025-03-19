ad36c6e2e76e856e8ad7d2249d181e10ff9a7684
Deepthi <jonnalagaddadeepthi96@gmail.com>
2025-03-19 21:28:07 +0530

    task

--- k8-eksctl/workstation.tf
+++ k8-eksctl/workstation.tf
@@ -6,13 +6,35 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   # key_name               = "user1"
   # monitoring             = true
-  #vpc_security_group_ids = [aws_security_group.allow_ekctl.id]
-  #subnet_id              = "subnet-01c1c823852d19a68"
+  vpc_security_group_ids = [aws_security_group.allow_ekctl.id]
+  subnet_id              = "subnet-01c1c823852d19a68"
   user_data = file("workstation.sh")
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "allow_ekctl" {
+  name        = "allow_ekctl"
+  description = "created for ekctl"
+  tags = {
+    Name = "allow_ekctl"
+  }
+  ingress {
+    description = "all ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
