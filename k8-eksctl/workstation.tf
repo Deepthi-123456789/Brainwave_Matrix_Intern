b0b8c7112a4172282b40a71a5f092a779593c7ad
Deepthi <jonnalagaddadeepthi96@gmail.com>
2025-03-19 21:32:22 +0530

    task

--- k8-eksctl/workstation.tf
+++ k8-eksctl/workstation.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   # key_name               = "user1"
   # monitoring             = true
-  vpc_security_group_ids = [aws_security_group.allow_ekctl.id]
+  vpc_security_group_ids = [aws_security_group.allow.id]
   subnet_id              = "subnet-02cc37d094c8acc11"
   user_data = file("workstation.sh")
 
@@ -16,7 +16,7 @@ module "ec2_instance" {
   }
 }
 
-resource "aws_security_group" "allow_ekctl" {
+resource "aws_security_group" "allow" {
   name        = "allow_ekctl"
   description = "created for ekctl"
   tags = {
